19edc537ff68855ee3214b426b42114213483033
DevDesignWin <debojitroy72005@gmail.com>
2026-01-04 14:21:38 +0530

    changes in db.js

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGO_URI}/ebookgenerator`, {});
-    console.log("Mongoose connected");
-  } catch (error) {
-    console.error("Error connecting to MongoDB", error);
-    process.exit(1);
-  }
+  mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  }).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 };
 
 module.exports = connectDB;
